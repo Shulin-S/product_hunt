13c74e6f1cf2da1ca57e1cf0875c0697c2389f96
Shulin <2741517596@qq.com>
2020-02-21 14:22:14 +0800

    添加了两个APP和主题模板

--- ph/urls.py
+++ ph/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import product.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', product.views.product_list),
 ]
